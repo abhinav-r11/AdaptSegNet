685468c5606bdfd17550e46ed44606ef0bc8e803
abhinav-r11 <abhinav.r1@tcs.com>
2024-09-03 11:39:17 +0530

    Create VisA_source.py

---
+++ dataset/VisA_source.py
@@ -0,0 +1,77 @@
+import os
+import os.path as osp
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import collections
+import torch
+import torchvision
+from torch.utils import data
+from PIL import Image
+
+
+class VisA_source(data.Dataset):
+    def __init__(self, root, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255):
+        self.root = root
+        self.crop_size = crop_size
+        self.scale = scale
+        self.ignore_label = ignore_label
+        self.mean = mean
+        self.is_mirror = mirror
+        # self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
+        self.img_ids = os.listdir(os.path.join(root,'images/'))
+        self.mask_ids = os.listdir(os.path.join(root,'masks/'))
+       
+        self.files = []
+
+        self.img_ids.sort()
+        self.mask_ids.sort()
+        # for split in ["train", "trainval", "val"]:
+        for iids,mids in zip(self.img_ids,self.mask_ids):
+            img_file = iids
+            label_file = mids
+            self.files.append({
+                "img": img_file,
+                "label": label_file,
+                "name": iids.split('/')[-1].split('.')[0]
+            })
+
+    def __len__(self):
+        return len(self.files)
+
+
+    def __getitem__(self, index):
+        datafiles = self.files[index]
+
+        image = Image.open(datafiles["img"]).convert('RGB')
+        label = Image.open(datafiles["label"])
+        name = datafiles["name"]
+
+        # resize
+        image = image.resize(self.crop_size, Image.BICUBIC)
+        label = label.resize(self.crop_size, Image.NEAREST)
+
+        image = np.asarray(image, np.float32)
+        label = np.asarray(label, np.float32)
+
+       
+
+        size = image.shape
+        image = image[:, :, ::-1]  # change to BGR
+        image -= self.mean
+        image = image.transpose((2, 0, 1))
+
+        return image.copy(), label_copy.copy(), np.array(size), name
+
+
+if __name__ == '__main__':
+    dst = VisA_source("./data", is_transform=True)
+    trainloader = data.DataLoader(dst, batch_size=4)
+    for i, data in enumerate(trainloader):
+        imgs, labels = data
+        if i == 0:
+            img = torchvision.utils.make_grid(imgs).numpy()
+            img = np.transpose(img, (1, 2, 0))
+            img = img[:, :, ::-1]
+            plt.imshow(img)
+            plt.show()
